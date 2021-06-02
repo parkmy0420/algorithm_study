1922e8ce40d77caef6a2f7b209474b070ea77a93
parkmy0420 <parkmy0420@gmail.com>
2021-06-02 17:14:50 +0900

    가운데 글자 가져오기

---
+++ programmers/level1/가운데 글자 가져오기.py
@@ -0,0 +1,12 @@
+s = 'qwer'
+
+def solution(s):
+    answer = ''
+    if len(s) % 2 == 1:
+        return s[len(s) // 2]
+    else:
+        return s[(len(s) // 2 - 1):(len(s) // 2 + 1)]
+
+    return answer
+
+print(solution(s))
