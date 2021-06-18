eb50fae600c5f1bc2cdb2379c3946defd2902140
parkmy0420 <parkmy0420@gmail.com>
2021-06-18 18:49:54 +0900

    소수 찾기

---
+++ programmers/level1/소수 찾기.py
@@ -0,0 +1,13 @@
+n = 10
+# n = 5
+
+def solution(n):
+    arr = [True] * (n+1)
+
+    for i in range(2, n+1):
+        for j in range(2*i, n+1, i):
+            arr[j] = False
+    return arr.count(True)-2
+# -2는 arr[0],arr[1]을 빼주기 위해서
+
+print(solution(n))
