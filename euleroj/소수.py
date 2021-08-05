f0fdba88748661ed8a06abc4b55892b609dcf895
parkmy0420 <parkmy0420@gmail.com>
2021-08-05 23:14:38 +0900

    소수

---
+++ euleroj/소수.py
@@ -0,0 +1,17 @@
+
+m = int(input())
+n = int(input())
+
+nnum = set(range(2,n+1))
+mnum = set(range(2,m))
+
+for i in range(2, int(n ** 0.5)+1):
+    if i in nnum:
+        nnum -= set(range(2*i, n+1, i))
+
+nnum = nnum - mnum
+if len(nnum) == 0:
+    print(-1)
+else:
+    print(sum(nnum))
+    print(min(nnum))
