f915f8fc3abc7c1250851189b6130ef58e5ad49c
parkmy0420 <parkmy0420@gmail.com>
2021-09-14 20:15:57 +0900

    소수 찾기 n범위 수정

--- programmers/level1/소수 찾기.py
+++ programmers/level1/소수 찾기.py
@@ -4,7 +4,7 @@ n = 10
 def solution(n):
     arr = [True] * (n+1)
 
-    for i in range(2, n+1):
+    for i in range(2, int(n**0.5)+1):
         for j in range(2*i, n+1, i):
             arr[j] = False
     return arr.count(True)-2
