2a1c16decfab41f85e933c3aeb67434df7789172
parkmy0420 <parkmy0420@gmail.com>
2021-10-03 02:22:43 +0900

    2535_아시아 정보올림피아드_ 수정

--- baekjoon/1주/2535_아시아 정보올림피아드.py
+++ baekjoon/1주/2535_아시아 정보올림피아드.py
@@ -1,24 +1,45 @@
 student_num = int(input())
-info, point, n_check = [], [], [0]
-
+info, point, n_check, now_n = [], [], [0], 0
 
 for i in range(student_num):
     a, b, c = map(int, input().split())
     info.append([a, b])
     point.append(c)
-    if len(n_check)-1 != a:
+    if a != now_n:
         n_check.append(0)
 
-for _ in range(2):
+while True:
     s_index = point.index(max(point))
-    print(f'{info[s_index][0]} {info[s_index][1]}')
-    n_check[info[s_index][0]] += 1
+    if n_check[info[s_index][0]] != 2:
+        print(f'{info[s_index][0]} {info[s_index][1]}')
+        if sum(n_check) != 2:
+            n_check[info[s_index][0]] += 1
+        else:
+            break
     point[s_index] = 0
 
-s_index = point.index(max(point))
-point[s_index] = 0
-if n_check[info[s_index][0]] == 2:
-    s_index = point.index(max(point))
-    print(f'{info[s_index][0]} {info[s_index][1]}')
-else:
-    print(f'{info[s_index][0]} {info[s_index][1]}')
+# 금, 은 구하고 동은 따로 구현한 경우
+# student_num = int(input())
+# info, point, n_check, now_n = [], [], [0], 0
+#
+# for i in range(student_num):
+#     a, b, c = map(int, input().split())
+#     info.append([a, b])
+#     point.append(c)
+#     if a != now_n:
+#         n_check.append(0)
+#
+# for _ in range(2):
+#     s_index = point.index(max(point))
+#     print(f'{info[s_index][0]} {info[s_index][1]}')
+#     n_check[info[s_index][0]] += 1
+#     point[s_index] = 0
+#
+# while tf:
+#     s_index = point.index(max(point))
+#     point[s_index] = 0
+#     if n_check[info[s_index][0]] == 2:
+#         tf = True
+#     else:
+#         print(f'{info[s_index][0]} {info[s_index][1]}')
+#         break
