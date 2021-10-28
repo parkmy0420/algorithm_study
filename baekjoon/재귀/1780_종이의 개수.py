68c3cb09ae3d7850f1aaa78c94177527c8bbc927
parkmy0420 <parkmy0420@gmail.com>
2021-10-28 22:23:37 +0900

    1780_종이의 개수_수정

--- baekjoon/재귀/1780_종이의 개수.py
+++ baekjoon/재귀/1780_종이의 개수.py
@@ -1,7 +1,34 @@
+# n = int(input())
+# matrix = [list(map(int, input().split())) for i in range(n)]
+#
+# def paper(matrix, n, s1, s2, cnt):
+#     start = matrix[s1][s2]
+#     if n != 1:
+#         for i in range(s1, s1+n):
+#             for j in range(s2, s2+n):
+#                 if matrix[i][j] != start:
+#                     for k in range(0, n, n//3): # 9등분하기
+#                         for l in range(0, n, n//3):
+#                             cnt = paper(matrix, n//3, s1+k, s2+l, cnt)
+#                     return cnt
+#
+#     if start == -1:
+#         cnt[0] += 1
+#     elif start == 0:
+#         cnt[1] += 1
+#     else:
+#         cnt[2] += 1
+#     return cnt
+#
+# cnt = paper(matrix, n, 0, 0, [0, 0, 0])
+# [print(cnt[i]) for i in range(3)]
+#
+#
 n = int(input())
 matrix = [list(map(int, input().split())) for i in range(n)]
+cnt = [0, 0, 0]
 
-def paper(matrix, n, s1, s2, cnt):
+def paper(n, s1, s2):
     start = matrix[s1][s2]
     if n != 1:
         for i in range(s1, s1+n):
@@ -9,7 +36,7 @@ def paper(matrix, n, s1, s2, cnt):
                 if matrix[i][j] != start:
                     for k in range(0, n, n//3): # 9등분하기
                         for l in range(0, n, n//3):
-                            cnt = paper(matrix, n//3, s1+k, s2+l, cnt)
+                            paper(n//3, s1+k, s2+l)
                     return cnt
 
     if start == -1:
@@ -20,5 +47,5 @@ def paper(matrix, n, s1, s2, cnt):
         cnt[2] += 1
     return cnt
 
-cnt = paper(matrix, n, 0, 0, [0, 0, 0])
+paper(n, 0, 0)
 [print(cnt[i]) for i in range(3)]
