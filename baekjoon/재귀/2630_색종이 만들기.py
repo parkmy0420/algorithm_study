0212ae508d580531b73b555fa32b165daa6757d7
parkmy0420 <parkmy0420@gmail.com>
2021-10-28 21:39:25 +0900

    2630_색종이 만들기_수정

--- baekjoon/재귀/2630_색종이 만들기.py
+++ baekjoon/재귀/2630_색종이 만들기.py
@@ -1,22 +1,22 @@
 n = int(input())
 paper = [list(map(int, input().split())) for i in range(n)]
-
-def colorpaper(paper, s1, e1, s2, e2, cnt):
+cnt = [0, 0]
+def colorpaper(s1, e1, s2, e2):
     start = paper[s1][e1]
     for i in range(s1, s2):
         for j in range(e1, e2):
             if paper[i][j] != start:    # 정사각형 색상이 모두 같지 않으면 4분할
-                cnt = colorpaper(paper, s1, e1, (s1+s2)//2, (e1+e2)//2, cnt)
-                cnt = colorpaper(paper, s1, (e1+e2)//2, (s1+s2)//2, e2, cnt)
-                cnt = colorpaper(paper, (s1+s2)//2, e1, s2, (e1+e2)//2, cnt)
-                cnt = colorpaper(paper, (s1+s2)//2, (e1+e2)//2, s2, e2, cnt)
-                return cnt
+                colorpaper(s1, e1, (s1+s2)//2, (e1+e2)//2)
+                colorpaper(s1, (e1+e2)//2, (s1+s2)//2, e2)
+                colorpaper((s1+s2)//2, e1, s2, (e1+e2)//2)
+                colorpaper((s1+s2)//2, (e1+e2)//2, s2, e2)
+                return
     if start == 1:  # 파란색 색종이
         cnt[1] += 1
     else:   # 하얀색 색종이
         cnt[0] += 1
     return cnt
 
-cnt = colorpaper(paper, 0, 0, n, n, [0, 0])
+colorpaper(0, 0, n, n)
 print(cnt[0])
 print(cnt[1])
