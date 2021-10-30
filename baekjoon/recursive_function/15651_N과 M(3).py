55fdcef652282b6b2a238c5bc0453ac3173c8b53
parkmy0420 <parkmy0420@gmail.com>
2021-10-30 17:22:59 +0900

    15651_N과 M(3)

---
+++ baekjoon/recursive_function/15651_N과 M(3).py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+nlist = [i for i in range(1, n+1)]
+temp = []
+
+def nm():
+    if len(temp) == m:
+        print(*temp)
+        return
+    for i in range(n):
+        temp.append(nlist[i])
+        nm()
+        temp.pop()
+if m == 1:
+    print(*nlist)
+else:
+    nm()
