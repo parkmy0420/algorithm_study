f8b30a2e11735f7797cad9b48aef3873983a59a5
parkmy0420 <parkmy0420@gmail.com>
2021-11-02 00:32:59 +0900

    15651_N과 M(3)_v2

--- baekjoon/recursive_function/15651_N과 M(3).py
+++ baekjoon/recursive_function/15651_N과 M(3).py
@@ -1,16 +1,29 @@
+# v1 - 재귀사용
+# n, m = map(int, input().split())
+# nlist = [i for i in range(1, n+1)]
+# temp = []
+#
+# def nm():
+#     if len(temp) == m:
+#         print(*temp)
+#         return
+#     for i in range(n):
+#         temp.append(nlist[i])
+#         nm()
+#         temp.pop()
+# if m == 1:
+#     print(*nlist, sep='\n')
+# else:
+#     nm()
+
+
+# v2 - product 사용
+from itertools import product
 n, m = map(int, input().split())
 nlist = [i for i in range(1, n+1)]
-temp = []
 
-def nm():
-    if len(temp) == m:
-        print(*temp)
-        return
-    for i in range(n):
-        temp.append(nlist[i])
-        nm()
-        temp.pop()
 if m == 1:
     print(*nlist, sep='\n')
 else:
-    nm()
+    for i in list(product(nlist, repeat=m)):
+        print(*i, sep=' ')
