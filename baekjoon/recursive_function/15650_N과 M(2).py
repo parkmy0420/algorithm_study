693132b7afd082ea94c1a7bbd352498bf59f33ac
parkmy0420 <parkmy0420@gmail.com>
2021-11-01 23:29:58 +0900

    15650_N과 M(2)_v2

--- baekjoon/recursive_function/15650_N과 M(2).py
+++ baekjoon/recursive_function/15650_N과 M(2).py
@@ -1,17 +1,30 @@
+# v1 - 재귀사용
+# n, m = map(int, input().split())
+# nlist = [i for i in range(1, n+1)]
+# temp = []
+#
+# def nm(start):
+#     if len(temp) == m:
+#         print(*temp)
+#         return
+#     for i in range(start, n):
+#         if nlist[i] not in temp:
+#             temp.append(nlist[i])
+#             nm(i+1)
+#             temp.pop()
+# if m == 1:
+#     print(*nlist, sep='\n')
+# else:
+#     nm(0)
+
+
+# v2 - 조합 사용(순서 고려 안함)
+from itertools import combinations
 n, m = map(int, input().split())
 nlist = [i for i in range(1, n+1)]
-temp = []
 
-def nm(start):
-    if len(temp) == m:
-        print(*temp)
-        return
-    for i in range(start, n):
-        if nlist[i] not in temp:
-            temp.append(nlist[i])
-            nm(i+1)
-            temp.pop()
 if m == 1:
     print(*nlist, sep='\n')
 else:
-    nm(0)
+    for i in list(combinations(nlist, m)):
+        print(*i, sep=' ')
