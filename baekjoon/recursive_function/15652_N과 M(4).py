c86861cee0ee25d7c64d1c61de17cf79427d59fd
parkmy0420 <parkmy0420@gmail.com>
2021-11-03 16:08:35 +0900

    15652_N과 M(4)_수정

--- baekjoon/recursive_function/15652_N과 M(4).py
+++ baekjoon/recursive_function/15652_N과 M(4).py
@@ -2,16 +2,28 @@ n, m = map(int, input().split())
 nlist = [i for i in range(1, n+1)]
 temp = []
 
-def nm():
+def nm(start):
     if len(temp) == m:
         print(*temp)
         return
-    for i in range(n):
-        if not temp or temp[-1] <= nlist[i]:
-            temp.append(nlist[i])
-            nm()
-            temp.pop()
+    for i in range(start, n):
+        temp.append(nlist[i])
+        nm(i)
+        temp.pop()
+
 if m == 1:
     print(*nlist, sep='\n')
 else:
-    nm()
+    nm(0)
+
+# v2 - product 사용 -> 메모리초과
+# from itertools import product
+# n, m = map(int, input().split())
+# nlist = [i for i in range(1, n+1)]
+#
+# if m == 1:
+#     print(*nlist, sep='\n')
+# else:
+#     for i in list(product(nlist, repeat=m)):
+#         if list(i) == sorted(i):
+#             print(*i, sep=' ')
