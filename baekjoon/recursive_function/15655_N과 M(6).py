ca0c1b0146c2402c0f2aaca62dafa0923ea7f748
parkmy0420 <parkmy0420@gmail.com>
2021-11-06 17:41:45 +0900

    15655_N과 M(6)_v2

--- baekjoon/recursive_function/15655_N과 M(6).py
+++ baekjoon/recursive_function/15655_N과 M(6).py
@@ -2,16 +2,28 @@ n, m = map(int, input().split())
 nlist = sorted(list(map(int,input().split())))
 temp = []
 
-def nm():
+def nm(start):
     if len(temp) == m:
         print(*temp)
         return
-    for i in range(n):
-        if not temp or temp[-1] < nlist[i]:
-            temp.append(nlist[i])
-            nm()
-            temp.pop()
+    for i in range(start, n):
+        temp.append(nlist[i])
+        nm(i+1)
+        temp.pop()
 if m == 1:
     print(*nlist, sep='\n')
 else:
-    nm()
+    nm(0)
+
+
+# import sys
+# from itertools import combinations
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+# nlist = sorted(list(map(int,input().split())))
+#
+# if m == 1:
+#     print(*nlist, sep='\n')
+# else:
+#     for i in list(combinations(nlist, m)):
+#         print(*i, sep=' ')
