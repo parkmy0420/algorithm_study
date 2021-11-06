d8f14059fa3160d40e30f9af7e80611557ee55fb
parkmy0420 <parkmy0420@gmail.com>
2021-11-06 17:19:49 +0900

    15654_N과 M(5)_v2

--- baekjoon/recursive_function/15654_N과 M(5).py
+++ baekjoon/recursive_function/15654_N과 M(5).py
@@ -15,3 +15,15 @@ if m == 1:
     print(*nlist, sep='\n')
 else:
     nm()
+
+# import sys
+# from itertools import permutations
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+# nlist = sorted(list(map(int,input().split())))
+#
+# if m == 1:
+#     print(*nlist, sep='\n')
+# else:
+#     for i in list(permutations(nlist, m)):
+#         print(*i, sep=' ')
