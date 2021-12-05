93e5cf0fcc89c452405622c71d916bae29ec464c
parkmy0420 <parkmy0420@gmail.com>
2021-12-06 02:11:56 +0900

    2206_벽 부수고 이동하기_수정

--- baekjoon/graph/2206.py
+++ baekjoon/graph/2206.py
@@ -2,18 +2,15 @@
 
 def bfs():
     q = deque()  # 시작 좌표
-    q.append((0, 0, 0, 1))
-    check[0][0][0] = 1  # 시작 좌표 노드 방문 표시
-    cnt = 0
+    q.append((0, 0, 0, 1))  # x좌표, y좌표, 벽 부순 여부, 이동 횟수
+    check[0][0] = [1, 0]  # 시작 좌표 노드 방문 표시
 
     while q:  # 큐가 빌 때까지
         x, y, bcnt, result = q.popleft()
         if x == n - 1 and y == m - 1:  # 마지막 지점에 도착하면 break
-            cnt += 1
-            print(result)
-            break
+            return result
 
-        for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+        for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:   # 상하좌우 이동
             nx, ny = x + dx, y + dy
             if 0 <= nx < n and 0 <= ny < m:
                 if check[nx][ny][0] == 0 or check[nx][ny][1] > bcnt:
@@ -24,15 +21,12 @@ def bfs():
                         check[nx][ny] = [1, bcnt+1]
                         q.append((nx, ny, bcnt+1, result+1))
 
-    if cnt == 0:    # 벽을 한번 부숴는데 더 이상 길이 없는 경우
-        print(-1)
+    return -1
 
 import sys
 from collections import deque
 input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().strip())) for _ in range(n)]
-relist = []
-
 check = [[[0, 0] for _ in range(m)] for _ in range(n)]  # [방문여부, 벽을 부쉈는지 여부]
-bfs()
+print(bfs())
