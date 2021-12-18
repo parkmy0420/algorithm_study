e8fbf4b89634bc2e305db26642a9d1283b849bdb
parkmy0420 <parkmy0420@gmail.com>
2021-12-18 17:40:11 +0900

    2747_피보나치 수

---
+++ baekjoon/Dynamic_Programming/2747.py
@@ -0,0 +1,14 @@
+# 2747 피보나치 수
+
+n = int(input())
+fibo = [0]*(n+1)
+
+for i in range(1, n+1):
+    if i == 0:
+        continue
+    elif i == 1:
+        fibo[1] = 1
+    else:
+        fibo[i] = fibo[i-1] + fibo[i-2]
+
+print(fibo[n])
