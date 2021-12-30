a4160392b8e4840af9379a6c444b70883dc4a3a3
parkmy0420 <parkmy0420@gmail.com>
2021-12-31 01:50:41 +0900

    12865_평범한 배낭_수정

--- baekjoon/Dynamic_Programming/12865.py
+++ baekjoon/Dynamic_Programming/12865.py
@@ -5,8 +5,8 @@ n, k = map(int, input().split())
 nlist = [list(map(int, input().split())) for _ in range(n)]
 check = [0] * (k + 1)
 
-for i in range(1, n + 1):
-    w, v = nlist[i - 1]
+for i in range(n):
+    w, v = nlist[i]
     for j in range(k, w-1, -1):
         check[j] = max(check[j], check[j-w]+v)
 print(check[-1])
