47c6235b9100ef05ce1dac714e4e69d88555771d
parkmy0420 <parkmy0420@gmail.com>
2022-01-20 14:22:54 +0900

    1920_수 찾기_v2(in 구현 추가)

--- baekjoon/Binary_Search/1920.py
+++ baekjoon/Binary_Search/1920.py
@@ -21,3 +21,14 @@ mlist = list(map(int, input().split()))
 
 for findnum in mlist:
     print(1 if binary(findnum) else 0)
+
+# # 파이썬 in 사용 ->  list -> 시간 초과, set -> 통과 시간 빠름
+# import sys
+# input = sys.stdin.readline
+# n = int(input())
+# nlist = set(map(int, input().split())) # list -> 시간 초과, set -> 통과 시간 빠름
+# m = int(input())
+# mlist = list(map(int, input().split()))
+#
+# for findnum in mlist:
+#     print(1 if findnum in nlist else 0)
