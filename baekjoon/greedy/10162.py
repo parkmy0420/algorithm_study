cd5b415ce5a4d64a091f64b61875f1abb907fbe3
parkmy0420 <parkmy0420@gmail.com>
2022-03-11 00:38:15 +0900

    10162_전자레인지

--- baekjoon/greedy/10162.py
+++ baekjoon/greedy/10162.py
@@ -7,8 +7,5 @@ for s in [300, 60, 10]:
     t, n = divmod(n, s)
     time.append(t)
 
-for i in time:
-    if n == 0: print(i, end=' ')
-    else:
-        print(-1)
-        break
+if n == 0: print(*time, sep=' ')
+else: print(-1)
