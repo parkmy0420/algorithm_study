3251fe5b9ab1ab0af551c3ea68bbc00b0e09344c
parkmy0420 <parkmy0420@gmail.com>
2022-04-04 18:41:47 +0900

    [2022KAKAOBLIND]신고결과받기_v2

--- programmers/level1/[2022KAKAOBLIND]신고결과받기.py
+++ programmers/level1/[2022KAKAOBLIND]신고결과받기.py
@@ -1,53 +1,27 @@
-# def solution(id_list, report, k):
-#     id_num = len(id_list)
-#     report_time = [0] * id_num
-#     user_report_list = [[] for _ in range(id_num)]
-#     sent = []
-#
-#     for i in report:    # 신고한 사람, 신고된 사람 정리
-#         a, b = i.split()
-#         n = id_list.index(a)
-#         if b not in user_report_list[n]:
-#             user_report_list[n].append(b)
-#             report_time[id_list.index(b)] += 1
-#
-#     for j in range(id_num):     # 정지된 유저 저장
-#         if report_time[j] >= k:
-#             sent.append(id_list[j])
-#
-#     report_time = [0] * id_num  # 초기화
-#     for k in range(id_num):     # 유저가 정지시킨 사람 수
-#         for stop_u in sent:
-#             if stop_u in user_report_list[k]:
-#                 report_time[k] += 1
-#
-#     return report_time
-
 def solution(id_list, report, k):
     id_num = len(id_list)
-    report_time = [0] * id_num
+    report_time = [0] * id_num  # index user가 신고받은 횟수 저장
     user_report_list = [[] for _ in range(id_num)]
-    sent = []
+    sent = []   # stop user list
+    report = list(set(report))
 
     for i in report:    # 신고한 사람, 신고된 사람 정리
         a, b = i.split()
-        n = id_list.index(a)
-        if b not in user_report_list[n]:
-            user_report_list[n].append(b)
-            report_time[id_list.index(b)] += 1
+        b_index = id_list.index(b)  # reported index
+        user_report_list[id_list.index(a)].append(b)    # reporter list
+        if report_time[b_index] == k-1:   # stop user append
+            sent.append(id_list[b_index])
+        else: report_time[b_index] += 1
 
-    for j in range(id_num):     # 정지된 유저 저장
-        if report_time[j] >= k:
-            sent.append(id_list[j])
 
-    report_time = [0] * id_num  # 초기화
-    for l in range(id_num):     # 유저가 정지시킨 사람 수
-        for stop_u in sent:
-            if stop_u in user_report_list[l]:
-                report_time[l] += 1
+    report_time = [0] * id_num  # # index user가 신고한 사람 중 정지당한 유저 수
+
+    for j in range(id_num):     # 유저가 정지시킨 사람 수
+        ucnt = len(set(user_report_list[j]) & set(sent))
+        report_time[j] = ucnt
 
     return report_time
 
-# print(solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","muzi apeach","neo apeach","apeach frodo","frodo neo","muzi neo","apeach muzi"],2))
+print(solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2))
 # print(solution(["con", "ryan"],["ryan con", "ryan con", "ryan con", "ryan con"],3))
-print(solution(['a','b','c','d','e','f'], ['a b', 'b c', 'a c', 'b d', 'b e', 'b d', 'e f', 'c e'], 2))
+# print(solution(['a','b','c','d','e','f'], ['a b', 'b c', 'a c', 'b d', 'b e', 'b d', 'e f', 'c e'], 2))
